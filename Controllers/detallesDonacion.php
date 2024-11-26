1ae8a062ad2b7e89cbdbb19068e3acf32286c637
Adrielunodostres <113220188+Adrielunodostres@users.noreply.github.com>
2024-11-26 10:29:55 -0600

    Donaciones admin

--- Controllers/detallesDonacion.php
+++ Controllers/detallesDonacion.php
@@ -10,8 +10,8 @@ $detalles = json_decode(file_get_contents('php://input'), true);
 $cantidad = $detalles['amt'];
 
 // Preparar la consulta SQL para insertar datos
-$sql = "INSERT INTO donaciones (monto)
-        VALUES ('$cantidad')";
+$sql = "INSERT INTO donaciones (monto, donante_id)  
+        VALUES ('$cantidad', '6')";
 
 // Ejecutar la consulta
 if ($conn->query($sql) === TRUE) {
